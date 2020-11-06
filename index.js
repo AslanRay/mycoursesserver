bd04f6347d3a98c8ac6b9308fe9c47785f4ca0db
Raymundo <aslan_ray@icloud.com>
2020-11-06 14:59:41 -0800

    API Swagger Documentation

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
 require('./database');
 const { DEFAULT_SERVER_PORT } = require('./utils/constants.js');
 const userRouters = require('./routes/user.router');
@@ -9,6 +10,7 @@ const courseRouters = require('./routes/course.router');
 const courseTypeRouters = require('./routes/courseType.router');
 const errorLoggRouters = require('./routes/errorLogg.router');
 const userCoursesTrackerRouters = require('./routes/userCoursesTracker.router');
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
 
@@ -33,6 +35,12 @@ courseTypeRouters.routersConfig(app);
 errorLoggRouters.routersConfig(app);
 
 userCoursesTrackerRouters.routersConfig(app);
+
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, { explorer: true }),
+);
 
 app.listen(app.get('Port'), () => {
   console.log(`Server started on port ${app.get('Port')}`);
